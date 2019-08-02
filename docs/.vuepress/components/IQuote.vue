ba57765ee579e7de627f542d1789c669b03beb7b
Rathawut Lertsuksakda <rathawut.l@gmail.com>
2019-08-02 08:57:25 +0700

    add nav & sidebar, init some articles

---
+++ docs/.vuepress/components/IQuote.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="tip custom-block text-center text-bold">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'IQuote'
+  }
+</script>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Usool.in.th',
-  description: 'อุศูล',
+  description: 'เว็บไซต์์ Usool.in.th',
   head: [
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Kanit' }],
   ],
@@ -8,4 +8,25 @@ module.exports = {
     '@vuepress/plugin-last-updated',
     require('./plugins/plugin-add-dir-auto'),
   ],
+  themeConfig: {
+    nav: [
+      { text: 'หน้าแรก', link: '/' },
+      { text: 'ปัญหาเห็นต่าง', link: '/kilaf/' },
+    ],
+    sidebar: [
+      {
+        title: 'ปัญหาเห็นต่าง',
+        path: '/kilaf/',
+        collapsable: false,
+        children: [
+          {
+            title: 'กุนูตซุบฮ์',
+            children: [
+              '/kilaf/qunoot/',
+            ],
+          },
+        ],
+      },
+    ],
+  },
 }
