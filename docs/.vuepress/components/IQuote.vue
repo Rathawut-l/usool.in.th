32996a54aafe37982019eea235419411205f5715
Rathawut Lertsuksakda <rathawut.l@gmail.com>
2019-08-02 15:43:53 +0700

    update IQuote, plugin-add-dir-auto, and some contents

--- docs/.vuepress/components/IQuote.vue
+++ docs/.vuepress/components/IQuote.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="tip custom-block text-center text-bold">
+  <div :class="{ tip: true, 'custom-block': true, 'text-center': center, 'text-bold': bold }">
     <slot></slot>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'IQuote'
-  }
+export default {
+  name: 'IQuote',
+  props: {
+    center: {
+      type: Boolean,
+      default: false,
+    },
+    bold: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
 </script>
--- docs/.vuepress/plugins/plugin-add-dir-auto/clientRootMixin.js
+++ docs/.vuepress/plugins/plugin-add-dir-auto/clientRootMixin.js
@@ -1,22 +1,22 @@
 export default {
   mounted () {
-    setTimeout(this.addDirAuto, 100);
+    setTimeout(this.addDirAuto, 500);
     this.$router.afterEach(() => {
-      setTimeout(this.addDirAuto, 100);
+      setTimeout(this.addDirAuto, 500);
     });
   },
   methods: {
     addDirAuto() {
       const content = document.querySelector('.page');
       if (!content) {
-        setTimeout(this.addDirAuto, 100);
+        setTimeout(this.addDirAuto, 500);
         return;
       }
-      const p = content.querySelectorAll('p'); 
+      const p = document.querySelectorAll('p'); 
       for (var i = 0; i < p.length; i++) {
         p[i].setAttribute('dir', 'auto');
       }
-      const blockquote = content.querySelectorAll('blockquote');
+      const blockquote = document.querySelectorAll('blockquote');
       for (let i = 0; i < blockquote.length; i++) {
         blockquote[i].setAttribute('dir', 'auto');
       }  
